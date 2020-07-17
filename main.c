52e30937c580abd25b07557b556cca9a681af346
renanrcs <renan.cavalcante@yahoo.com>
2020-07-17 11:27:37 -0300

    Meu primeiro commit em C

---
+++ main.c
@@ -0,0 +1,14 @@
+//Introdução Variavaeis;
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int var1, var2, var3;
+    var1 = var2 = var3 = 4;
+
+    printf("Valor de var1 %i\n", var1);
+    printf("Valor de var2 %i\n", var2);
+    printf("Valor de var3 %i\n", var3);
+    return 0;
+}
